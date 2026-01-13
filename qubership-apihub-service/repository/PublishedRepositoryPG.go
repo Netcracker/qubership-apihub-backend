d3688db70045a82953b8a148c1f513332dbded8b
Aleksandr Karpov <aleksandr.v.karpov.qubership@gmail.com>
2026-01-13 08:38:48 +0300

    fix: linter errors

--- qubership-apihub-service/repository/PublishedRepositoryPG.go
+++ qubership-apihub-service/repository/PublishedRepositoryPG.go
@@ -173,7 +173,7 @@ func (p publishedRepositoryImpl) PatchVersion(packageId string, versionName stri
 			calculateLiteSearchOperationsQuery := `
 								insert into fts_latest_release_operation_data
 								select o.package_id, o.version, o.revision, o.operation_id, o.type, to_tsvector(convert_from(od.data,'UTF-8'))  data_vector from
-		                        	operation o inner join operation_data od on o.data_hash=od.data_hash
+								operation o inner join operation_data od on o.data_hash=od.data_hash
 									where package_id = ? and version = ? and revision = ?
 								on conflict (package_id, version, revision, operation_id) do update set data_vector = EXCLUDED.data_vector;`
 			_, err = tx.Exec(calculateLiteSearchOperationsQuery,
@@ -1450,7 +1450,7 @@ func (p publishedRepositoryImpl) CreateVersionWithData(packageInfo view.PackageI
 			calculateLiteSearchOperationsQuery := `
 						insert into fts_latest_release_operation_data
 						select o.package_id, o.version, o.revision, o.operation_id, o.type, to_tsvector(convert_from(od.data,'UTF-8'))  data_vector from
-                        	operation o inner join operation_data od on o.data_hash=od.data_hash
+						operation o inner join operation_data od on o.data_hash=od.data_hash
 							where package_id = ? and version = ? and revision = ?
 						on conflict (package_id, version, revision, operation_id) do update set data_vector = EXCLUDED.data_vector;`
 			_, err = tx.Exec(calculateLiteSearchOperationsQuery,
