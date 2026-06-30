a4b1b5862867f9cd6f4329e81fc96ddd525a891f
dmuratkan <damir.muratkan@netcracker.com>
2026-06-30 15:24:11 +0500

    cover auth-check and PATCH moved-package redirect

--- qubership-apihub-service/controller/PackageExportConfigController.go
+++ qubership-apihub-service/controller/PackageExportConfigController.go
@@ -35,7 +35,7 @@ func (p packageExportConfigControllerImpl) GetConfig(w http.ResponseWriter, r *h
 	packageId := getStringParam(r, "packageId")
 	sufficientPrivileges, err := p.roleService.HasRequiredPermissions(ctx, packageId, view.ReadPermission)
 	if err != nil {
-		utils.RespondWithError(w, "Failed to check user privileges", err)
+		handlePkgRedirectOrRespondWithError(w, r, p.ptHandler, packageId, "Failed to check user privileges", err)
 		return
 	}
 	if !sufficientPrivileges {
@@ -106,7 +106,7 @@ func (p packageExportConfigControllerImpl) SetConfig(w http.ResponseWriter, r *h
 
 	err = p.expConfSvc.SetConfig(packageId, req.AllowedOasExtensions)
 	if err != nil {
-		utils.RespondWithError(w, "Failed to update package export config", err)
+		handlePkgRedirectOrRespondWithError(w, r, p.ptHandler, packageId, "Failed to update package export config", err)
 		return
 	}
 
